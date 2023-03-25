7511477a0363018246adb178ec8ac29fa33f0f50
Niels <111445696+NielsNL1@users.noreply.github.com>
2023-03-25 18:48:55 +0100

    Update mainv0.2.py

--- mainv0.2.py
+++ mainv0.2.py
@@ -17,6 +17,9 @@ keyboard.add_hotkey('0', stop_loop)
 
 running = True
 
+# This is so that you have time to open the browser, it is not nessacry and you can delelte this line of code if you want.
+time.sleep(5)
+
 while running:
     # You might need to change this depending on what browser you use. 400 = x 125 = y
     pyautogui.click(400,125)
